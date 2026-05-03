4723e22d3b06397ed6297e10c375196341d16c77
Ciwa Fps <tizuses123@gmail.com>
2026-05-03 20:02:31 +0700

    •••

--- CiwaBeta.lua
+++ CiwaBeta.lua
@@ -1,29 +1,27 @@
 if _G.CiwaLoading then 
     return
 end
+
 _G.CiwaLoading = true
-local VERSION = "1.0"
+
 local HUB_NAME = "CiwaHub"
 local games = {
     [6739698191] = "https://raw.githubusercontent.com/CiwaFps/Ciw-/refs/heads/main/Game/Ciwa%20x%20VD.lua",
 }
 local UNIVERSAL_SCRIPT = "https://raw.githubusercontent.com/CiwaFps/Ciw-/refs/heads/main/Game/Uwu.lua"
-local universeId = game.GameId
-local scriptURL  = games[universeId]
-if scriptURL then
-    local ok, err = pcall(function()
-        loadstring(game:HttpGet(scriptURL))()
-    end)
-    if not ok then
-        warn(string.format("[%s] Gagal load script khusus: %s", HUB_NAME, tostring(err)))
-    end
-else
-    local ok, err = pcall(function()
-        loadstring(game:HttpGet(UNIVERSAL_SCRIPT))()
-    end)
-    if not ok then
-        warn(string.format("[%s] Gagal load script bebas: %s", HUB_NAME, tostring(err)))
-    end
+
+-- Gunakan PlaceId jika ID di atas adalah ID dari URL game
+local currentId = game.PlaceId
+local scriptURL = games[currentId] or UNIVERSAL_SCRIPT
+
+local ok, err = pcall(function()
+    loadstring(game:HttpGet(scriptURL))()
+end)
+
+if not ok then
+    warn(string.format("[%s] Gagal load script: %s", HUB_NAME, tostring(err)))
 end
+
+-- Tunggu sebentar sebelum mengizinkan eksekusi ulang
 task.wait(5)
 _G.CiwaLoading = false
