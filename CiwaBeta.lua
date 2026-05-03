1a4d9e102ad680ef440b4e7b30791c629362ed79
Ciwa Fps <tizuses123@gmail.com>
2026-05-03 20:11:44 +0700

    Repair

--- CiwaBeta.lua
+++ CiwaBeta.lua
@@ -1,24 +1,31 @@
 if _G.CiwaLoading then 
     return
 end
-
 _G.CiwaLoading = true
-
 local HUB_NAME = "CiwaHub"
+
 local games = {
     [6739698191] = "https://raw.githubusercontent.com/CiwaFps/Ciw-/refs/heads/main/Game/Ciwa%20x%20VD.lua",
 }
 local UNIVERSAL_SCRIPT = "https://raw.githubusercontent.com/CiwaFps/Ciw-/refs/heads/main/Game/Uwu.lua"
 
-local currentId = game.GameId
-local scriptURL = games[currentId] or UNIVERSAL_SCRIPT
+local universeId = game.GameId
+local scriptURL = games[universeId]
 
-local ok, err = pcall(function()
-    loadstring(game:HttpGet(scriptURL))()
-end)
-
-if not ok then
-    warn(string.format("[%s] Gagal load script: %s", HUB_NAME, tostring(err)))
+if scriptURL then
+    local ok, err = pcall(function()
+        loadstring(game:HttpGet(scriptURL))()
+    end)
+    if not ok then
+        warn(string.format("[%s] Gagal load script khusus: %s", HUB_NAME, tostring(err)))
+    end
+else
+    local ok, err = pcall(function()
+        loadstring(game:HttpGet(UNIVERSAL_SCRIPT))()
+    end)
+    if not ok then
+        warn(string.format("[%s] Gagal load script universal: %s", HUB_NAME, tostring(err)))
+    end
 end
 
 task.wait(5)
